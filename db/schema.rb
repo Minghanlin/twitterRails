c9d9b8bed4eb180b4f9494b096696511f98a7108
Minghan Lin <cd>
2016-07-25 17:14:55 +0800

    hello rails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160725073232) do
+ActiveRecord::Schema.define(version: 20160725084004) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
